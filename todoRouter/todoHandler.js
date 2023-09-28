211cd00b0a490266f4256eb5b8bb5453107e3456
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-28 16:14:51 +0600

    created post api using mongoose method

--- todoRouter/todoHandler.js
+++ todoRouter/todoHandler.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const todoSchema = require("../schemas/todoSchema");
+const Todo = require("../schemas/todoSchema");
+
 // get all the todo's
 router.get("/", async () => {});
 
 // post  the todo
-router.post("/", async () => {});
+router.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body);
+  await newTodo.save((err) => {
+    if (err) {
+      res.status(500).json({
+        error: "Something is wrong",
+      });
+    } else {
+      res.status(200).json({
+        message: "Todo inserted successful!",
+      });
+    }
+  });
+});
 
 // post multiple todo
 router.post("/all", async () => {});
