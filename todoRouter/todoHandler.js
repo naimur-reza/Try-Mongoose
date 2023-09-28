759835f64690b28e386c7e1844015c51a0f89b77
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-28 17:59:31 +0600

    added : try catch validation  in all async func

--- todoRouter/todoHandler.js
+++ todoRouter/todoHandler.js
@@ -4,45 +4,65 @@ const Todo = require("../schemas/todoSchema");
 
 // get all the todo's
 router.get("/", async (req, res) => {
-  const filter = { status: "active" }; // use filter if we need to get specific condition data
-  const result = await Todo.find(filter).select({ _id: 1, title: 1 }); // use select if we need just specific fields
-  res.send(result);
+  try {
+    const filter = { status: "active" }; // use filter if we need to get specific condition data
+    const result = await Todo.find(filter).select({ _id: 1, title: 1 }); // use select if we need just specific fields
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // post  the todo
 router.post("/", async (req, res) => {
-  const newTodo = new Todo(req.body);
-  await newTodo.save();
-  res.send(newTodo);
+  try {
+    const newTodo = new Todo(req.body);
+    await newTodo.save();
+    res.send(newTodo);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // post multiple todo
 router.post("/all", async (req, res) => {
-  await Todo.insertMany(req.body);
-  res.send(req.body);
+  try {
+    await Todo.insertMany(req.body);
+    res.send(req.body);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // update the todo
 router.put("/:id", async (req, res) => {
-  const result = await Todo.findByIdAndUpdate(
-    { _id: req.params.id },
-    {
-      $set: {
-        ...req.body,
+  try {
+    const result = await Todo.findByIdAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          ...req.body,
+        },
       },
-    },
-    {
-      new: true,
-      runValidators: true,
-    }
-  );
-  res.send(result);
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // delete the todo
 router.delete("/:id", async (req, res) => {
-  await Todo.deleteOne({ _id: req.params.id });
-  res.status(200).send("Deleted");
+  try {
+    await Todo.deleteOne({ _id: req.params.id });
+    res.status(200).send("Deleted");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
