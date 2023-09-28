f82f6c72723e5f9419947d18dc2b842997a6c691
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-28 18:55:30 +0600

    add: instance steps

--- __steps__.js
+++ __steps__.js
@@ -3,5 +3,6 @@
 2. Create a model with this schema
 3. Create  a instance from the model
 4. Then use operation from the instance
+5. For create a instance method we have to use methods in schema, and then create a model and return the function
 
 `;
